a0102b859e5f17392eaee9cfaa97a37eb81553a7
James Kim <eodnjs2998@gmail.com>
2015-01-04 17:36:01 +0900

    tileset 추가. home 추가.

---
+++ TMX/Interior general.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Interior general" tilewidth="16" tileheight="16">
+ <image source="../Assets/Tileset/Interior general.PNG" width="128" height="4016"/>
+ <terraintypes>
+  <terrain name="RugGreen" tile="-1"/>
+  <terrain name="TableBlue" tile="-1"/>
+ </terraintypes>
+ <tile id="784" terrain=",,,0"/>
+ <tile id="785" terrain=",,0,0"/>
+ <tile id="786" terrain=",,0,"/>
+ <tile id="792" terrain=",0,,0"/>
+ <tile id="793" terrain="0,0,0,0"/>
+ <tile id="794" terrain="0,,0,"/>
+ <tile id="800" terrain=",0,,"/>
+ <tile id="801" terrain="0,0,,"/>
+ <tile id="802" terrain="0,,,"/>
+ <tile id="1699" terrain=",,,1"/>
+ <tile id="1700" terrain=",,1,1"/>
+ <tile id="1701" terrain=",,1,"/>
+ <tile id="1707" terrain=",1,,"/>
+ <tile id="1708" terrain="1,1,,"/>
+ <tile id="1709" terrain="1,,,"/>
+</tileset>
