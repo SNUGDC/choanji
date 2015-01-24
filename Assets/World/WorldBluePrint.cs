9a448c0a9b2e4dbbc7eadcaa125d406ab29eb8ef
James Kim <eodnjs2998@gmaill.com>
2015-01-24 12:12:40 +0900

    *.DotSettings 추가. WorldBluePrint Read 추가. 샘플 월드 home.json 추가.

--- Assets/World/WorldBluePrint.cs
+++ Assets/World/WorldBluePrint.cs
@@ -8,8 +8,8 @@ namespace Choanji
 	{
 		public class Room
 		{
-			public Point pos;
 			public MapMeta meta;
+			public Point pos;
 		}
 
 		private readonly List<Room> mRooms = new List<Room>();
@@ -27,5 +27,31 @@ namespace Choanji
 			return mRectGroup.Overlaps(_rect).Select(_idx => mRooms[_idx]).ToList();
 		}
 
+		private static readonly Path_ JSON_PATH = new Path_("Resources/World");
+
+		// todo: Read binary when release.
+		public static WorldBluePrint Read(string _world)
+		{
+			var _ret = new WorldBluePrint();
+
+			var _roomsJs = JsonHelper.DataWithRaw(JSON_PATH / (_world + ".json"));
+			if (_roomsJs == null) return null;
+			foreach (var _roomJs in _roomsJs.GetListEnum())
+			{
+				MapMetaAndGrid _map;
+				var _mapID = MapIDHelper.Make((string) _roomJs["map"]);
+
+				if (!MapDB.TryGet(_mapID, out _map))
+					continue;
+
+				Point _pos;
+				if (!Point.TryParse(_roomJs["pos"], out _pos))
+					continue;
+				
+				_ret.Add(new Room { meta = _map.meta, pos = _pos });
+			}
+
+			return _ret;
+		}
 	}
 }
