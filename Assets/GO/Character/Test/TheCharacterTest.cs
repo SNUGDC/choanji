c4eb8e898c0692612f62216bf35aeb4fedc1b7d8
James Kim <eodnjs2998@gmail.com>
2015-01-26 22:54:17 +0900

    TheCharacter, TheCharacterTest 추가.

---
+++ Assets/GO/Character/Test/TheCharacterTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Choanji
+{
+
+	public class TheCharacterTest : MonoBehaviour
+	{
+		public Character ch;
+
+		void Start()
+		{
+			TheCharacter.g = ch;
+		}
+	}
+
+
+}
---
+++ Assets/GO/Character/TheCharacter.cs
@@ -0,0 +1,28 @@
+﻿using Gem;
+using UnityEngine;
+
+namespace Choanji
+{
+	public static class TheCharacter
+	{
+		private static Character mG;
+
+		public static Character g
+		{
+			get { return mG; }
+			set
+			{
+				if (g != null)
+				{
+					L.W(L.M.SHOULD_NULL("g"));
+					Object.Destroy(g.GetComponent<CharacterInputAgent>());
+				}
+
+				mG = value;
+				D.Assert(g.GetComponent<CharacterInputAgent>() == null);
+				g.gameObject.AddComponent<CharacterInputAgent>();
+			}
+		}
+	}
+
+}
