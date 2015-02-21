f86d47f5daaeeea395fec3fcc5546728e08e235e
James Kim <eodnjs2998@gmail.com>
2015-02-21 22:28:16 +0900

    생성자 개선.

--- Assets/TriggerAction/Action/ActionOpenDialog.cs
+++ Assets/TriggerAction/Action/ActionOpenDialog.cs
@@ -11,14 +11,21 @@ namespace Choanji
 		public ActionOpenDialog(JsonData _json)
 			: base(ActionType.OPEN_DIALOG)
 		{
-			dialog = (string) _json["dialog"];
-			blockMove = _json.BoolOrDefault("block_move", true);
+			if (_json.IsString)
+			{
+				dialog = (string)_json;
+			}
+			else
+			{
+				dialog = (string)_json["dialog"];
+				blockMove = _json.BoolOrDefault("block_move", true);
+			}
 		}
 
 		public override void Do(object _data)
 		{
 			TheCharacter.ctrl.blockMove.Add(CharacterMoveBlock.INSPECTEE);
-			TheDialog.g.Open(new DialogProvider(DialogHelper.FullPath(dialog)), new DialogHandler()
+			TheDialog.g.Open(DialogHelper.MakeProvider(dialog), new DialogHandler()
 			{
 				onDone = delegate
 				{
