e88551eb0239e934d2488fa315ddfd8ff67a4dd2
James Kim <eodnjs2998@gmail.com>
2015-02-22 15:27:15 +0900

    card detail added.

--- Assets/UI/Deck/DeckPopup.cs
+++ Assets/UI/Deck/DeckPopup.cs
@@ -1,9 +1,23 @@
 ﻿using Gem;
+using UnityEngine;
 
 namespace Choanji.UI
 {
 	public class DeckPopup : Popup
 	{
 		public PartyTab partyTab;
+
+		public RectTransform cardDetailParent;
+
+		[HideInInspector]
+		public CardDetail cardDetail;
+
+		void Start()
+		{
+			cardDetail = DB.g.cardDetailPrf.Instantiate();
+			var _rect = cardDetail.gameObject.RectTransform();
+			_rect.SetParent(cardDetailParent, false);
+			_rect.Fill();
+		}
 	}
 }
